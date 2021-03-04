5c4fa762364ad2e36d08e38622fb5a9bb20a7d83
masumi-soga <sg.kse777@gmail.com>
2021-03-04 10:06:35 +0000

    [Finish]応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,11 +6,7 @@ class BookCommentsController < ApplicationController
 		@book_comment = BookComment.new(book_comment_params)
 		@book_comment.book_id = @book.id
 		@book_comment.user_id = current_user.id
-		if @book_comment.save
-  		redirect_to book_path(@book.id)
-		else
-		  render 'books/show'
-		end
+		@book_comment.save
 	end
 
 	def destroy
